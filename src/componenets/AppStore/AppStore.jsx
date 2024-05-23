00fc3e6c50c415a6da1a89c391f2e4895cc25d34
Bassem <bassem.bendhaou@hotmail.com>
2024-05-23 21:54:00 +0200

    dark mode for app store's section

--- src/componenets/AppStore/AppStore.jsx
+++ src/componenets/AppStore/AppStore.jsx
@@ -4,11 +4,11 @@ import mobileBike from "../../assets/mobile_bike.gif";
 
 export default function AppStore() {
   return (
-    <div className="p-14 bg-gray-100">
+    <div className="p-14 bg-gray-100 dark:bg-gray-800">
       <div className="container">
         <div className="grid grid-cols-1 sm:grid-cols-2">
           <div className="flex flex-col max-w-xl gap-4">
-            <h1 className="text-xl sm:text-4xl font-semibold text-gray-700 text-center sm:text-left">
+            <h1 className="text-xl dark:text-gray-400 sm:text-4xl font-semibold text-gray-700 text-center sm:text-left">
               Foodly is Available for Android and IOS
             </h1>
             <div className="flex flex-wrap mt-5 justify-center sm:justify-start">
