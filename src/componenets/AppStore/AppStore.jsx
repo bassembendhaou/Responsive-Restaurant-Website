eda192ed7c1d1022d22d29c08851c444330f8d9c
Bassem <bassem.bendhaou@hotmail.com>
2024-05-24 11:00:42 +0200

    finish work

--- src/componenets/AppStore/AppStore.jsx
+++ src/componenets/AppStore/AppStore.jsx
@@ -6,7 +6,7 @@ export default function AppStore() {
   return (
     <div className="p-14 bg-gray-100 dark:bg-gray-800">
       <div className="container">
-        <div className="grid grid-cols-1 sm:grid-cols-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-14">
           <div className="flex flex-col max-w-xl gap-4">
             <h1 className="text-xl dark:text-gray-400 sm:text-4xl font-semibold text-gray-700 text-center sm:text-left">
               Foodly is Available for Android and IOS
